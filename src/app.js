286a45cb279a866452a249178bc0350cab1eda7a
nishb369 <nishbcodes@gmail.com>
2024-12-25 16:43:36 +0530

    Featured Categories Section Addded

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import FeatureCardsSection from "./Components/FeatureCardsSection/FeatureCardsSection"
+import FeaturedCategoriesSection from "./Components/FeaturedCategoriesSection/FeaturedCategoriesSection"
 
 const App = () => {
   return (
     <>
       <Header />
       <FeatureCardsSection />
+      <FeaturedCategoriesSection/>
       <Footer />
     </>
   );
